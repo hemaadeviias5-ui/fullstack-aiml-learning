fef42936ee6caa5f664f4f48db4902c0060e57c2
hemaadeviias5-ui <hemaadeviias5@gmail.com>
2026-03-05 19:49:23 +0400

    day 1 code

---
+++ Fullstack/html-css/day1/script.js
@@ -0,0 +1,13 @@
+let name = "Hemaa";
+
+function greet() {
+    console.log("Hello " + name);
+}
+
+greet();
+
+const skills = ["React","Spring Boot","Python","Machine Learning"];
+
+skills.map(skill => {
+    console.log("Skill:", skill);
+});
